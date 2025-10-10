ac4f2a348706e94451b7ac71b8d3202d28d9a18d
고민균 <97932282+skyblue1232@users.noreply.github.com>
2025-10-11 04:28:12 +0900

    fix/button

--- Loopy-fe/src/pages/SplashPage.tsx
+++ Loopy-fe/src/pages/SplashPage.tsx
@@ -2,7 +2,12 @@ import LoopyLogo from "../assets/images/LoopyLogo.svg?react";
 
 const SplashPage = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen animate-gradient text-white text-center -mx-[1.5rem]">
+    <div
+      className="flex flex-col items-center justify-center h-screen text-white text-center -mx-[1.5rem]"
+      style={{
+        background: "linear-gradient(135deg, #6970F3 0%, #3D418D 100%)",
+      }}
+    >
       <h1 className="font-bold text-[1.5rem] leading-[150%]">
         오늘의 커피, 매일의 루피
       </h1>
