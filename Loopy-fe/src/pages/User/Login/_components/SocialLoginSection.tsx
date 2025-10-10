7b42cb1aba6a22b3f42d20c008651fa2d37bda09
고민균 <97932282+skyblue1232@users.noreply.github.com>
2025-10-11 05:23:13 +0900

    fix/social-login

--- Loopy-fe/src/pages/User/Login/_components/SocialLoginSection.tsx
+++ Loopy-fe/src/pages/User/Login/_components/SocialLoginSection.tsx
@@ -18,7 +18,7 @@ const SocialLoginSection = () => {
   };
 
   return (
-    <div className="relative z-0 flex flex-col items-center mt-[3rem]">
+    <div className="flex flex-col items-center mt-[1.5rem]">
       <div className="flex items-center w-full pointer-events-none">
         <div className="flex-grow h-px bg-[#DFDFDF] mr-[0.75rem]" />
         <span className="text-[#DFDFDF] font-semibold text-[0.75rem] whitespace-nowrap">
