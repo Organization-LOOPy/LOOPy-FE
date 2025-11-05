02f0125361d3764bad419f9fc189eb1101ab1854
고민균 <97932282+skyblue1232@users.noreply.github.com>
2025-11-05 18:15:52 +0900

    refactor/direct-update

--- Loopy-fe/src/hooks/mutation/my/exchange/useConvertStampBook.ts
+++ Loopy-fe/src/hooks/mutation/my/exchange/useConvertStampBook.ts
@@ -1,15 +1,31 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { postConvertStampBook } from "../../../../apis/my/exchange/api";
-import type { ApiResponse, ConvertStampBookResponse } from "../../../../apis/my/exchange/type";
+import type {
+  ApiResponse,
+  ConvertStampBookResponse,
+} from "../../../../apis/my/exchange/type";
+import type { StampBookItem } from "../../../../apis/my/myStampbook/type";
 
 export const useConvertStampBook = (
   onSuccess?: (data: ApiResponse<ConvertStampBookResponse>) => void,
   onError?: (error: unknown) => void
 ) => {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationKey: ["convertStampBook"],
     mutationFn: (stampBookId: number) => postConvertStampBook(stampBookId),
-    onSuccess: (data) => onSuccess?.(data),
+
+    onSuccess: (data, stampBookId) => {
+      queryClient.setQueryData<StampBookItem[]>(["myStampBooks"], (old) =>
+        old ? old.filter((book) => book.id !== stampBookId) : []
+      );
+
+      queryClient.invalidateQueries({ queryKey: ["myStampBooks"] });
+
+      onSuccess?.(data);
+    },
+
     onError,
   });
 };
